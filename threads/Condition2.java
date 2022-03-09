125fcf0a3b9cd7c405f0ab4a30df868c41aea9d9
Arsh <arsh.pelia1@gmail.com>
2022-03-08 21:10:45 -0500

    Added Comments

--- threads/Condition2.java
+++ threads/Condition2.java
@@ -87,12 +87,21 @@ public class Condition2 {
 		}
 	}
 
+	
+	/*
+	 * Invoked by the ThreadedKernel class to ensure correct implementation
+	 */
 	public static void selfTest(){    
 		Test1();
 		Test2();
 		new Test3();
+		//Test4();
 	}
 	
+	/*
+	 * First test begins by testing locks as well as simple debugging of sleep,
+	 * wake and wakeAll function using 10 threads. 
+	 */
 	public static void Test1(){
 		
 		System.out.print("\nCommencing Condition2 Test 1: Sleep & Wake \n\n");
@@ -100,12 +109,12 @@ public class Condition2 {
 	    Condition2 condition = new Condition2(lock); 
 
 	    KThread threads[] = new KThread[10];
-		for (int i=0; i<10; i++) {
-	         threads[i] = new KThread(new test1Locks(lock, condition));
+		for (int i=0; i<10; i++) { //initialize 10 threads to be tested
+	         threads[i] = new KThread(new test1Locks(lock, condition));//calls test1Locks to execute thread functionality
 	         threads[i].setName("Thread-" + i).fork();
 		}
 
-	    KThread.yield();
+	    KThread.yield(); // relinquish processor
 	    
 	    lock.acquire();
 
@@ -119,13 +128,16 @@ public class Condition2 {
 
 	    System.out.print("\n Terminating Condition2 Test 1 by releasing Locks. \n\n");	
 
-	    threads[9].join();
+	    threads[9].join(); // wait until threads terminate
 	    System.out.print("\n Condition2 Test 1: Successful \n\n");
 
 	}
 	
+	/*
+	 * Implements threads for testing conditions and then terminating
+	 */
 	private static class test1Locks implements Runnable {
-		test1Locks(Lock lock, Condition2 condition) {
+		test1Locks(Lock lock, Condition2 condition) { //initialize condition variable and lock
 		    this.condition = condition;
 	        this.lock = lock;
 		}
@@ -143,6 +155,10 @@ public class Condition2 {
 	    private Condition2 condition; 
 	}
 
+	/*
+	 * Uses a linked list to test performance and reliability of Condition2 testing.
+	 * Context Switching is also tested via producer and consumer threads. 
+	 */
 	public static void Test2(){
 		
 		System.out.print("\nCommencing Condition2 Test 2: Context Switching \n\n");
@@ -157,9 +173,9 @@ public class Condition2 {
                 while(intList.isEmpty()){
                     cond.sleep();
                 }
-                System.out.print("\nEnsuring list size = 5\n");	
-                Lib.assertTrue(intList.size() == 5);
-                System.out.print("\nList size = 5\n");	
+                System.out.print("\nEnsuring list size = 50\n");	
+                Lib.assertTrue(intList.size() == 50);
+                System.out.print("\nList size = 50\n");	
                 
                 while(!intList.isEmpty()) {
                     KThread.yield();
@@ -172,7 +188,7 @@ public class Condition2 {
         KThread producer = new KThread( new Runnable () {
             public void run() {
                 lock.acquire();
-                for (int i = 0; i < 5; i++) {
+                for (int i = 0; i < 50; i++) {
                     intList.add(i);
                     System.out.println("Thread: " + i + " was Added");
                     KThread.yield();
@@ -188,18 +204,27 @@ public class Condition2 {
         consumer.fork();
         producer.fork();
         
+        //Wait until all processes finish execution
+        
         consumer.join();
         System.out.print("\nTerminating Condition2 Test 2 \n\n");	
         producer.join();
         
         System.out.print("\nCondition2 Test 2: Successful \n\n");
     }
-	   
+	
+	/*
+	 * Test conducts by executing threads in a procedural manner. In short, each thread
+	 * will run alternatively (i.e. after one another) via condition implementation
+	 */
 	private static class Test3{
 		
 		private static Lock lock;
 		private static Condition2 cond;
 		
+		/*
+		 * Initialize 5 pings and 5 pong threads. 
+		 */
 		private static class pingTester implements Runnable{
 			public void run(){
 				lock.acquire();
@@ -224,13 +249,17 @@ public class Condition2 {
 
 			ping.fork();
 			pong.fork();
-
+			/*
+			 * In order for pon to execute, it must wait until ping is terminated 
+			 * via join and vice versa. Otherwise, an indefinite block may occur.
+			 */
 			for (int i = 0; i < 50; i++) { KThread.currentThread().yield(); }
 			System.out.print("\nCondition2 Test 3: Successful \n\n");
 		}
 	}
 	
-	
 	private Lock conditionLock; // lock to implement condition
 	private LinkedList<KThread> waitQueue; //A number of threads sleeping on condition
+
+
 }
