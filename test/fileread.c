670e39b559212f86775690c04a1522f4303933b1
Anthony Milad <aoemilad@gmail.com>
2022-04-12 21:42:16 -0400

    Task 2 Tests

---
+++ test/fileread.c
@@ -0,0 +1,22 @@
+#include "syscall.h"
+#include "stdlib.h"
+#include "stdio.h"
+
+int main(){
+
+	char testfile[] = {'t','e','s','t','f','i','l','e','.','t','x','t'};
+	char *filepointer = &testfile[0];
+	int filedescriptor = open(filepointer);
+	
+	printf("This is the file: %d\n", filedescriptor);
+	
+	char array[24];
+	char *buffer = &array[0];
+	int bytes = read(filedescriptor, buffer, 24);
+
+	printf("We have read this many bytes: %d\n", bytes);
+	printf("Contents of the test file:\n%s\n", array);
+
+	return 0;
+}
+	
