3afd7d2978a132dac81666a938becd13869345b5
Nikholas Pcenicni <npcenicni@laurentian.ca>
2022-04-16 16:41:04 -0400

    added gregs test

--- test/syscallTests.c
+++ test/syscallTests.c
@@ -1,8 +1,5 @@
 #include "stdio.h"
-#include "stdlib.h"
-
-
-#define NUMTESTS       2        
+#include "stdlib.h"        
 
 void log(char *format, ...);
 void route(int, char);
@@ -23,7 +20,7 @@ int main(int argc, char *argv[]) {
         route(test, dbg_flag);
     } else {
         printf("File Syscall Test run all tests: \n");
-        for (i = 0; i < NUMTESTS; i++) {
+        for (i = 0; i < 4; i++) {
             printf("File Syscall Test run the %d test\n", i);
             route(i, dbg_flag);
         }
@@ -94,7 +91,66 @@ void route(int test, char dbg_flag) {
 
             printf("File Syscall Test 2: Completed successfully \n");
             break;
-        
+
+        case 2:
+            printf("File Syscall Test 3: Started \n");
+            char fileName0[] = {'f','.','c'};
+            char *namePtr0 = &fileName0[0];
+
+            char fileName1[] = {'f','.','c','o','f','f'};
+            char *namePtr1 = &fileName1[0];
+
+            printf("File Syscall Test 3: Performing an Exec on file name without a .coff extension \n");
+            int *r0;
+            int a0 = exec(namePtr0,0,null);
+            join(a0, r0);
+
+            printf("File Syscall Test 3: Performing an Exec on file name with a .coff extension \n");
+            int *r1;
+            int a1 = exec(namePtr1,0,null);
+            join(a1, r1);
+            printf("File Syscall Test 3: Complete \n");
+
+            break;
+
+        case 3:
+            printf("File Syscall Test 4: Started \n");
+            printf("File Syscall Test 4: Creating process 1 \n");
+            char fileName1[] = {'j','1','.','c','o','f','f'};
+            char *ptr1 = &fileName1[0];
+            
+            printf("File Syscall Test 4: Creating process 2 \n");
+            char fileName2[] = {'j','2','.','c','o','f','f'};
+            char *ptr2 = &fileName2[0];
+
+            int status1;
+            int status2;
+            
+            int *r1 = &status1;
+            int PID1 = exec(ptr1,0,null);
+
+            char a = (char)PID1;
+            char *args[1];
+            args[0] = &a;
+
+            int PID2 = exec(ptr2,1,args);
+            int *r2 = &status2;
+            
+            printf("File Syscall Test 4: Joining process 2 with process 1 (non-child process) \n");
+            join(PID2, r2);
+            join(PID1, r1);	
+            printf("File Syscall Test 4: PID1: %d\n PID2: %d\n", PID1, PID2);
+            
+            printf("File Syscall Test 4: Complete \n");
+
+            printf("File Syscall Test 5: Started \n");
+            printf("File Syscall Test 5: Exiting \n");		
+            exit(-1);
+            printf("File Syscall Test 5: Checking Process IDs to ensure they are null \n");
+            printf("File Syscall Test 5: PID1: %d\n PID2: %d\n", PID1, PID2);
+            printf("File Syscall Test 5: Complete \n");
+
+            break;
     }
 
 };
